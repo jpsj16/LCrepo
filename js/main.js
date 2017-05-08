68bf03c709245bce43e1fe401260b459bfb73525
BOS USER <BOS USER>
2017-05-08 00:05:57 -0700

    ok

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
 angular
   .module("triviaQestion", [])
-// var a = document.getElementById("questionAmount").value
-// var b = document.getElementById("questionCategory").value
-// var c = document.getElementById("questionDifficulty").value
-// var d = document.getElementById("questionType").value
-// alert(a)
+
   //responsible for all function in angularjs
   .controller("triviaCtrl", function($scope, $http) {
     $scope.$watch("questionAmount", function() {
-            // returnSearch();
       });
       $scope.$watch("questionCategory", function() {
-            // returnSearch();
         $scope.$watch("questionDifficulty", function() {
-            // returnSearch();
         });
           $scope.$watch("questionType", function() {
              returnSearch();
@@ -23,15 +16,13 @@ angular
       $http.get("https://opentdb.com/api.php?amount=" + $scope.questionAmount + "&category=" + $scope.questionCategory +"&difficulty=" + $scope.questionDifficulty + "&type=" + $scope.questionType).then(function(p) {
           console.log(p.data.results);
           //regular expression converter
-          // $scope.triviaQestion = p.data.results;
+
           let array = p.data.results;
           array.forEach(function(item) {
             item.question = item.question.replace(/\&.{4}\;/g, "'");
            item.question = item.question.replace(/\&.{5}\;/g, '"');
            item.question = item.question.replace(`&#039;/g`, '');
-            // item.incorrect_answers = item.incorrect_answers.replace(`&#039;`, '');
-           // item.incorrect_answers = item.incorrect_answers.replace(/[""]/g, " ");
-      //      item.question = item.question.remove(/\["\;/g);
+
           });
           $scope.trivias = array;
 
@@ -39,7 +30,3 @@ angular
         });
     }
   });
-//   $('#ok').click(function(){
-//         alert(array1)
-
-//   })
