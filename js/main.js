365f7d1aae06f452ec4ed653e88e7f470a51ce2f
boslagu <rlagurin.bos@gmail.com>
2017-05-09 03:32:37 -0700

    updated

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 function AppCtrl($scope) {
   $scope.currentNavItem = 'Quiz';
 }
@@ -6,29 +5,31 @@ function AppCtrl($scope) {
 angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
 
   .controller("QuizCtrl", function($scope, $http) {
-    $scope.$watch("amount", function() {
+
+      $scope.$watch("type", function() {
+        returnSearch();
       });
-      $scope.$watch("category", function() {
-        $scope.$watch("difficulty", function() {
-        });
-          $scope.$watch("type", function() {
-             returnSearch();
-          });
-    });
+
 
     function returnSearch() {
-      $http.get("https://opentdb.com/api.php?amount=" + $scope.amount + "&category=" + $scope.category +"&difficulty=" + $scope.difficulty + "&type=" + $scope.type).then(function(p) {
-          console.log(p.data);
-          //regular expression converter
-          // $scope.triviaQestion = p.data.results;
-          let array = p.data.results;
-          array.forEach(function(item) {
-            item.question = item.question.replace(/\&.{4}\;/g, "'");
-           item.question = item.question.replace(/\&.{5}\;/g, '"');
-           item.question = item.question.replace(`&#039;/g`, '');
-          });
-          $scope.trivias = array;
-          //end of regular expression converter
+      $http.get("https://opentdb.com/api.php?amount=" + $scope.amount + "&category=" + $scope.category + "&difficulty=" + $scope.difficulty + "&type=" + $scope.type).then(function(p) {
+        console.log(p.data);
+        //regular expression converter
+        // $scope.triviaQestion = p.data.results;
+        let array = p.data.results;
+        array.forEach(function(item) {
+          item.question = item.question.replace(/\&.{4}\;/g, "'");
+          item.question = item.question.replace(/\&.{5}\;/g, '"');
+          item.question = item.question.replace(`&#039;/g`, '');
         });
+        $scope.trivias = array;
+        //end of regular expression converter
+      });
     }
+
+
+  })
+
+  .controller("AboutCtrl", function($scope) {
+    $scope.title = 'ABOUT PAGE'
   });
