3be0752df11e2ea6ec20e19c182d4cb8e3695aa8
boslagu <rlagurin.bos@gmail.com>
2017-05-11 09:51:35 +0800

    Updated all Codes

--- js/main.js
+++ js/main.js
@@ -1,20 +1,19 @@
+
 function AppCtrl($scope) {
   $scope.currentNavItem = 'Quiz';
 }
 
 angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-
   .controller("QuizCtrl", function($scope, $http) {
-
       $scope.$watch("type", function() {
         returnSearch();
       });
 
-
     function returnSearch() {
+
       $http.get("https://opentdb.com/api.php?amount=" + $scope.amount + "&category=" + $scope.category + "&difficulty=" + $scope.difficulty + "&type=" + $scope.type).then(function(p) {
-        console.log(p.data.response_code);
-        //regular expression converter
+        console.log(p.data);
+
         let array = p.data.results;
         array.forEach(function(item) {
           item.question = item.question.replace(/\&.{4}\;/g, "'");
@@ -22,16 +21,14 @@ angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
           item.question = item.question.replace(`&#039;/g`, '');
         });
         $scope.trivias = array;
-        //end of regular expression converter
       });
     }
-
-
   })
 
   .controller("AboutCtrl", function($scope) {
     $scope.title = 'ABOUT PAGE'
   });
+
 
 
 //   function TimeCtrl($scope, $timeout) {
