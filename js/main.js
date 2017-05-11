0272dba0a70aa71d622a2108d4469eb9cf8c1b83
boslagu <rlagurin.bos@gmail.com>
2017-05-11 15:32:58 +0800

    questionChecker update

--- js/main.js
+++ js/main.js
@@ -1,33 +1,76 @@
 
+
+var responseCode
+var messageDisplay
 function AppCtrl($scope) {
   $scope.currentNavItem = 'Quiz';
 }
 
 angular.module('MyApp', ['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
   .controller("QuizCtrl", function($scope, $http) {
+
+function returnSearch() {
+  $http.get("https://opentdb.com/api.php?amount=" + $scope.amount + "&category=" + $scope.category + "&difficulty=" + $scope.difficulty + "&type=" + $scope.type).then(function(p) {
+    console.log(p.data);
+    responseCode = p.data.response_code
+    let array = p.data.results;
+    array.forEach(function(item) {
+      item.question = item.question.replace(/\&.{4}\;/g, "'");
+      item.question = item.question.replace(/\&.{5}\;/g, '"');
+      item.question = item.question.replace(`&#039;/g`, '');
+    });
+    $scope.trivias = array;
+  checkQuestions();
+    // respnseCode = response_code
+    // alert(responseCode)
+  });
+}
+
       $scope.$watch("type", function() {
         returnSearch();
+      // alert(messageDisplay)
       });
 
-    function returnSearch() {
-
-      $http.get("https://opentdb.com/api.php?amount=" + $scope.amount + "&category=" + $scope.category + "&difficulty=" + $scope.difficulty + "&type=" + $scope.type).then(function(p) {
-        console.log(p.data);
-
-        let array = p.data.results;
-        array.forEach(function(item) {
-          item.question = item.question.replace(/\&.{4}\;/g, "'");
-          item.question = item.question.replace(/\&.{5}\;/g, '"');
-          item.question = item.question.replace(`&#039;/g`, '');
-        });
-        $scope.trivias = array;
-      });
+    function checkQuestions(){
+      if (responseCode === 0){
+        messageDisplay = "Ready?"
+      }else if (responseCode === 1){
+        messageDisplay = "No Questions available."
+      }else if (responseCode ===2){
+        messageDisplay = "No Questions available."
+      }
     }
   })
 
-  .controller("AboutCtrl", function($scope) {
-    $scope.title = 'ABOUT PAGE'
+  .controller("AboutCtrl", function($scope, $mdDialog) {
+    // $scope.$watch("open", function() {
+    //   alert(messageDisplay)
+    // });
+
+    $scope.showAlert = function(ev) {
+      if (messageDisplay === "No Questions available."){
+      $mdDialog.show(
+        $mdDialog.alert()
+          .parent(angular.element(document.querySelector('#popupContainer')))
+          .clickOutsideToClose(true)
+          .title('There is no question available.')
+          // .textContent('You can specify some description text in here.')
+          // .ariaLabel('Alert Dialog Demo')
+          .ok('Got it!')
+          .targetEvent(ev)
+      );
+    }else if (messageDisplay === "Ready?"){
+(".open")
+    }
+    };
+
+
+
+
+
   });
+
+
 
 
 
